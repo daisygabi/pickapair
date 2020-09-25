1ad515be4824ce6daa81eb1ea0b4f5df70d13683
Gabi <gabrielaradu.developer@gmail.com>
2020-09-25 22:43:12 +0300

    UI changes and cleanup.

--- src/components/ShowPairs.js
+++ src/components/ShowPairs.js
@@ -7,7 +7,7 @@ class ShowPairs extends Component {
 		return (
 			<div className="col-12">
 				{this.props.pairs.length > 0 ? this.props.pairs.map((item, key) => {
-					return <div className="post-block p-3 m-3 flex-column shadow" key={key}>
+					return <div className="info-block p-3 m-3 flex-column shadow" key={key}>
 						<p className="d-flex align-items-center justify-content-center">{item.firstPerson} + {item.secondPerson}</p>
 					</div>
 				}) : <div><img src={require('../img/undraw_pair.svg')} alt="" className="p-3 m-3"/></div>}
