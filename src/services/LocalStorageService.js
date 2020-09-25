55e6af5462a3050739b10ea751e12b0c3b1f0abf
Gabi <gabrielaradu.developer@gmail.com>
2020-09-25 22:21:37 +0300

    Save input and results in localstorage.

---
+++ src/services/LocalStorageService.js
@@ -0,0 +1,14 @@
+export class LocalStorageService {
+
+	retrieveItemFromLocalStorage = (key) => {
+		return localStorage && localStorage.getItem(key);
+	}
+
+	saveItemToLocalStorage = (key, value) => {
+		localStorage && localStorage.setItem(key, value);
+	}
+
+	clear() {
+		localStorage && localStorage.clear();
+	}
+}
