19f9d4f489b365216380b846bb9e4c293eb2aaa2
Gabi <gabrielaradu.developer@gmail.com>
2020-09-26 13:22:27 +0300

    Align center results in mobile screen.

--- src/components/ShowPairs.js
+++ src/components/ShowPairs.js
@@ -6,9 +6,9 @@ class ShowPairs extends Component {
 	render() {
 		return (
 			<div className="row">
-				<div className="col-12">
+				<div className="col-12 d-flex flex-wrap justify-content-center">
 					{this.props.pairs.length > 0 ? this.props.pairs.map((item, key) => {
-						return <div className="d-inline-block info-block p-3 m-3 shadow" key={key}>
+						return <div className="d-inline-block flex-grow-0 flex-shrink-0 info-block p-3 m-3 shadow" key={key}>
 							<div className="d-flex align-items-center justify-content-center h-100"><span>{item.firstPerson} + {item.secondPerson}</span></div>
 						</div>
 					}) : <div className="row"><img src={require('../img/undraw_pair.svg')} alt="" className="col-12"/>
