249f0c0344826f3a15b0d53a9a5666ea4567777c
Gabi <gabrielaradu.developer@gmail.com>
2020-09-26 13:06:18 +0300

    Center align results in layout.

--- src/components/ShowPairs.js
+++ src/components/ShowPairs.js
@@ -5,12 +5,15 @@ class ShowPairs extends Component {
 
 	render() {
 		return (
-			<div className="col-12">
-				{this.props.pairs.length > 0 ? this.props.pairs.map((item, key) => {
-					return <div className="info-block p-3 m-3 flex-column shadow" key={key}>
-						<p className="d-flex align-items-center justify-content-center">{item.firstPerson} + {item.secondPerson}</p>
-					</div>
-				}) : <div><img src={require('../img/undraw_pair.svg')} alt="" className="p-3 m-3"/></div>}
+			<div className="row">
+				<div className="col-12">
+					{this.props.pairs.length > 0 ? this.props.pairs.map((item, key) => {
+						return <div className="d-inline-block info-block p-3 m-3 shadow" key={key}>
+							<div className="d-flex align-items-center justify-content-center h-100"><span>{item.firstPerson} + {item.secondPerson}</span></div>
+						</div>
+					}) : <div className="row"><img src={require('../img/undraw_pair.svg')} alt="" className="col-12"/>
+					</div>}
+				</div>
 			</div>
 		);
 	}
